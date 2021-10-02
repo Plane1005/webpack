86037cdcc38205b9ec647abc6ef6a71336b8ce3b
Jet1005 <1352351506@qq.com>
2021-10-02 10:38:53 +0800

    day2

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader','postcss-loader']
+        use: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },'postcss-loader']
       },{
         test: /\.less/,
         use: ['style-loader', 'css-loader','postcss-loader','less-loader']
