19d4cb1b47bae1b58128641b0029e5725b8ab541
Jet1005 <1352351506@qq.com>
2021-11-17 22:52:50 +0800

    workdetail

--- src/page/WorkDetail/index.tsx
+++ src/page/WorkDetail/index.tsx
@@ -5,6 +5,8 @@ import { RootState, useAppDispatch } from '@/store'
 import { useSelector } from 'react-redux'
 import { fetchWorkDetail } from '@/store/reducer/systemReducer'
 import './style.less'
+import { Avatar, Button } from 'antd'
+import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined'
 
 const WorkDetail: React.FC = (props: any) => {
   let workId = props?.location?.state?.workId || null
@@ -15,14 +17,29 @@ const WorkDetail: React.FC = (props: any) => {
   useEffect(() => {
     dispatch(
       fetchWorkDetail({
-        workId: workId
+        workId: workId,
       })
     )
   }, [])
 
-  return (<div className="g-workList">
-    123
-  </div>)
+  return (
+    <div className="g-workDetail">
+      <div className="m-workDetail">
+        <div className="m-left">
+          <div className="m-hrDetail">
+            <Avatar src={workDetail.hrAvatar} size={60} />
+            <div className="m-hrInfo">
+              <div className="u-name">{workDetail.hrName}</div>
+              <div className="u-info">
+                招聘 <em className="vdot">·</em> 刚刚活跃
+              </div>
+            </div>
+            <Button type="primary" icon={<PlusOutlined />} className="u-btn" >向Ta投递简历</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default WorkDetail
