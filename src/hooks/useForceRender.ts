1294c820916a2d0a1f005bb83b2f6c885477b3b1
Jet <1352351506@qq.com>
2022-12-08 00:15:03 +0800

    feat: hooks

---
+++ src/hooks/useForceRender.ts
@@ -0,0 +1,11 @@
+import React, { useCallback, useState } from 'react'
+
+export default function useForceRender() {
+  const [count, setCount] = useState(0)
+
+  const forceRender = useCallback(() => {
+    setCount((i) => i + 1)
+  }, [])
+
+  return [forceRender, count]
+}
---
+++ src/hooks/useSearchParams.ts
@@ -0,0 +1,7 @@
+import React, { useMemo } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function useSearchParams() {
+  const { search } = useLocation()
+  return useMemo(() => new URLSearchParams(search), [search])
+}
